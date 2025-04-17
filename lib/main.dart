717750c22bce1df8a0bace5135ddae7ae51bc926
Owen Lin <owen4work@gmail.com>
2025-04-17 16:25:24 +0800

    #7 - Stateless Widgets & Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,41 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('First Flutter App'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower',
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Floating Action Button Pressed!');
-          },
-          child: Text('Click'),
-          backgroundColor: Colors.red[600],
-        ),
-      ),
+      home: Home(),
     ),
   );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Flutter App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Floating Action Button Pressed!');
+        },
+        backgroundColor: Colors.red[600],
+        child: Text('Click'),
+      ),
+    );
+  }
 }
