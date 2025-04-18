eedb0deaf2ea258ee428113919bde58a3fb49119
Owen Lin <owen4work@gmail.com>
2025-04-18 14:42:28 +0800

    #19 - Cards

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,29 @@ class _MyAppState extends State<MyApp> {
     ),
   ];
 
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+            ),
+            SizedBox(height: 6),
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 14, color: Colors.grey[800]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,10 +63,7 @@ class _MyAppState extends State<MyApp> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children:
-            quotes
-                .map((quote) => Text('${quote.text} - ${quote.author}'))
-                .toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
