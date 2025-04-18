2c150fe0087086154bdb268119830cb6b151782e
Owen Lin <owen4work@gmail.com>
2025-04-18 17:37:35 +0800

    #26 - Flutter Packages (http)

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/home',
+      initialRoute: '/',
       routes: {
         '/': (context) => const Loading(),
         '/home': (context) => const Home(),
--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,27 +8,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
-
-  void getData() async {
-    String username = await Future.delayed(const Duration(seconds: 3), () {
-      return 'Owen';
-    });
-
-    String bio = await Future.delayed(const Duration(seconds: 3), () {
-      return 'web developer';
-    });
-
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('initState() called');
-  }
-
   @override
   Widget build(BuildContext context) {
     print('build() called');
@@ -40,14 +19,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: const Text('Choose a location'),
         centerTitle: true,
         elevation: 0,
-      ),
-      body: ElevatedButton(
-        onPressed: () {
-          setState(() {
-            counter++;
-          });
-        },
-        child: Text('Counter: $counter'),
       ),
     );
   }
